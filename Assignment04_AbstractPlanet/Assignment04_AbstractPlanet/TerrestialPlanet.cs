2417fe51a453b6520b2a49375597965d19534b65
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 17:45:33 -0400

    Added constructor

--- Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/TerrestialPlanet.cs
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/TerrestialPlanet.cs
@@ -8,18 +8,28 @@ using System.Threading.Tasks;
  * Name: Akshay Patel
  * 300918514
  * Comp 123 Assignment 04 ABSTRACT PLANET
- * Version 2.1 - Added private instance variable.
+ * Version 2.2 - Added Constructor.
  */
 namespace Assignment04_AbstractPlanet
 {
     /// <summary>
     /// This is a public TerrestialPlanet subclass.
     /// </summary>
-    public class TerrestialPlanet
+    public class TerrestialPlanet : Planet
     {
         // Private Instance Variables
         private bool _oxygen;
-        
+
         // Constructor =========================================
+        ///<summary>
+        ///This is the constructor of TerrestialPlanet subclass.
+        /// It takes 5 arguments - name(string), diameter(double), mass(double), oxygen(bool), moonCount(int).
+        /// From which 3  arguments name, diameter and mass are derived from Planet class.
+        /// </summary>
+        public TerrestialPlanet(string name, double diameter, double mass, bool oxygen, int moonCount) : base (name, diameter, mass)
+        {
+            _oxygen = oxygen;
+            
+        } 
     }
 }
