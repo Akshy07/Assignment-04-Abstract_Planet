fad0fd61a1096af4dd75f9094876712a928d2e2d
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 16:51:53 -0400

    Added properties to Public properties

--- Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/Planet.cs
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/Planet.cs
@@ -21,10 +21,91 @@ namespace Assignment04_AbstractPlanet
         private int _moonCount;
         private string _name;
         private double _orbitalPeriod;
-        private int _ringCoount;
+        private int _ringCount;
         private double _rotationPeriod;
 
         // Public Properties
+        /// <summary>
+        /// These are readonly properties
+        /// </summary>
+        
+        //Diameter
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+
+        //Mass
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+
+        //MoonCount
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+        }
+
+        //Name
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+        
+        /// <summary>
+        /// This has values for the set.
+        /// </summary>
+        //OrbitalPeriod
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        //RingCount
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        // RotationPeriod
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        } 
 
         // Constructors =============================================
 
