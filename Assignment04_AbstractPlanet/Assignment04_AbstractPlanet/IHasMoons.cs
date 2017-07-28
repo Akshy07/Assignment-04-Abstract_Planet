eba4b63631f1ce231bd1cc6561594242cad16e3f
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 17:54:53 -0400

    Created IHasMoons Interface

---
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/IHasMoons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ * This is a public Interface method
+ */
+namespace Assignment04_AbstractPlanet
+{
+    public interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
