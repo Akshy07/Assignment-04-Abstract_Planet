e3b6e7fe8d42606d71bd507b238d4514a201475e
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 17:37:46 -0400

    Created TerrestialPlanet subclass.

---
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/TerrestialPlanet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ * Version 2.0 - Created TerrestialPlanet subclass.
+ */
+namespace Assignment04_AbstractPlanet
+{
+    /// <summary>
+    /// This is a public TerrestialPlanet subclass.
+    /// </summary>
+    public class TerrestialPlanet
+    {
+        // Private Instance Variables
+
+        // Constructor =========================================
+    }
+}
