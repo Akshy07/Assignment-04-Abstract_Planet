edd7ce6486ca6b0c226f991b45a4b7dcb651148c
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 18:00:17 -0400

    Created IHasRings interface method

---
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/IHasRings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ * Version C - Created IHasRings Interface method.
+ */
+namespace Assignment04_AbstractPlanet
+{
+    interface IHasRings
+    {
+        bool HasRings();
+    }
+}
