84fee750e02822ed4de796218d40508af49d73ed
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 17:14:32 -0400

    Created  GiantPlanet public subclass

--- Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/GiantPlanet.cs
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/GiantPlanet.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ * Version 1.0 - This is a public GiantPlanet subclass. 
+ */
+
 namespace Assignment04_AbstractPlanet
 {
-    class GiantPlanet
+    /// <summary>
+    /// This is a public GiantPlanet subclass.
+    /// </summary>
+    public class GiantPlanet
     {
+        // Private Instance Variables
+
+        //Constructor =========================================
     }
 }
