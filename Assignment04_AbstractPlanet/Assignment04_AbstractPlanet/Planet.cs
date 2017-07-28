1b46578826ef90fc5154e5d73677c2ca379593fd
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 16:16:50 -0400

    Created Abstract Plane Class

---
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ * Version 0.1 - Created Abstract Planet class.
+ */
+
+namespace Assignment04_AbstractPlanet
+{
+    abstract class Planet
+    {
+        // Private Instance Variables
+
+        // Public Properties
+
+        // Constructors =============================================
+
+        // Override Method
+    }
+}
---
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ */
+
+namespace Assignment04_AbstractPlanet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
