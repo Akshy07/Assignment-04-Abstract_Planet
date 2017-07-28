058985bd7cc4da5eed349a904e3c75000aeb7bc3
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 17:58:28 -0400

    Created IHabitable interface method.

---
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/IHabitable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Name: Akshay Patel
+ * 300918514
+ * Comp 123 Assignment 04 ABSTRACT PLANET
+ * Version B - Created IHabitable interface method.
+ */
+namespace Assignment04_AbstractPlanet
+{
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
