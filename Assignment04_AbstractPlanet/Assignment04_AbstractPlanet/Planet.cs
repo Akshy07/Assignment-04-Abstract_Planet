f0a1721f925f4a104c3027648a01761206a60bde
Akshy07 <apate583@my.centennialcollege.ca>
2017-07-28 16:38:45 -0400

    Added Private Instance Variables

--- Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/Planet.cs
+++ Assignment04_AbstractPlanet/Assignment04_AbstractPlanet/Planet.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Akshay Patel
  * 300918514
  * Comp 123 Assignment 04 ABSTRACT PLANET
- * Version 0.1 - Created Abstract Planet class.
+ * Version 0.2 - Added Private Instance Variables .
  */
 
 namespace Assignment04_AbstractPlanet
@@ -16,6 +16,13 @@ namespace Assignment04_AbstractPlanet
     abstract class Planet
     {
         // Private Instance Variables
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCoount;
+        private double _rotationPeriod;
 
         // Public Properties
 
